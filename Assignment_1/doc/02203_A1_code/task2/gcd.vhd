67b8e0f24d5f1a5924380b545d6ebe274c87b8c7
Flymardones <42536234+Flymardones@users.noreply.github.com>
2024-09-28 11:52:42 +0200

    Update gcd.vhd

--- Assignment_1/doc/02203_A1_code/task2/gcd.vhd
+++ Assignment_1/doc/02203_A1_code/task2/gcd.vhd
@@ -37,7 +37,7 @@ begin
     next_state <= state;
     next_reg_b <= reg_b;
     next_reg_a <= reg_a;
-    ack <= '0';  -- Default value, will be set in specific states
+    ack <= '0';  
     
     case (state) is
     
@@ -47,44 +47,44 @@ begin
           end if;
         
         when load_a =>
-          next_reg_a <= AB;               -- Assign AB input to reg_a
-          ack <= '1';                     -- Acknowledge that reg_a is loaded
-          if req = '0' then               -- Wait for req to go low
+          next_reg_a <= AB;               
+          ack <= '1';                     
+          if req = '0' then               
             ack <= '0';
             next_state <= load_b;
           end if;
          
         when load_b =>
           if req = '1' then 
-            next_reg_b <= AB;             -- Assign AB input to reg_b
+            next_reg_b <= AB;             
             next_state <= compare;
           end if;
         
         when compare =>
           if reg_a = 0 then
-            next_reg_a <= reg_b;          -- If reg_a is 0, set reg_a to reg_b
+            next_reg_a <= reg_b;          
             next_state <= done;
           elsif reg_b = 0 then
-            next_state <= done;           -- If reg_b is 0, GCD is found
+            next_state <= done;           
           elsif reg_a = reg_b then
-            next_state <= done;           -- If reg_a equals reg_b, GCD is found
+            next_state <= done;           
           elsif reg_a > reg_b then
-            next_state <= subtract_a;      -- Subtract reg_b from reg_a if reg_a is larger
+            next_state <= subtract_a;      
           else
-            next_state <= subtract_b;      -- Subtract reg_a from reg_b otherwise
+            next_state <= subtract_b;      
           end if;
         
         when subtract_a =>
-          next_reg_a <= reg_a - reg_b;     -- Perform subtraction: reg_a - reg_b
+          next_reg_a <= reg_a - reg_b;     
           next_state <= compare;
         
         when subtract_b =>
-          next_reg_b <= reg_b - reg_a;     -- Perform subtraction: reg_b - reg_a
+          next_reg_b <= reg_b - reg_a;     
           next_state <= compare;
         
         when done =>
-          ack <= '1';                      -- Set ack to indicate computation is complete
-          C <= reg_a;                      -- Output the GCD in reg_a
+          ack <= '1';                      
+          C <= reg_a;                      
           next_state <= idle;
           
         when others =>
@@ -98,13 +98,13 @@ begin
   seq : process (clk, reset)
   begin
     if reset = '1' then
-      state <= idle;                      -- Reset state to idle
-      reg_a <= (others => '0');           -- Clear reg_a
-      reg_b <= (others => '0');           -- Clear reg_b
+      state <= idle;                      
+      reg_a <= (others => '0');           
+      reg_b <= (others => '0');           
     elsif rising_edge(clk) then
-      state <= next_state;                -- Update state
-      reg_a <= next_reg_a;                -- Update reg_a
-      reg_b <= next_reg_b;                -- Update reg_b
+      state <= next_state;                
+      reg_a <= next_reg_a;                
+      reg_b <= next_reg_b;                
     end if;
   end process seq;
 
