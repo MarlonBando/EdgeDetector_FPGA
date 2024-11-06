9f299d98006a8f3cb4c69a08fc04f366c78609cd
Christopher <christophermardonesandersen@gmail.com>
2024-11-06 17:16:20 +0100

    Intial work on vhdl for task 1-3

--- Assignment_2/02203_A2_code/task2/acc2.vhd
+++ Assignment_2/02203_A2_code/task2/acc2.vhd
@@ -27,7 +27,9 @@ use work.types.all;
 
 entity acc is
     generic(
-        MAX_ADDR : unsigned(15 downto 0) := to_unsigned(25343, 16)
+        MAX_ADDR : unsigned(15 downto 0) := to_unsigned(25343, 16);
+        MAX_ROW  : unsigned(15 downto 0) := to_unsigned(288, 16);
+        MAX_COL  : unsigned(15 downto 0) := to_unsigned(352, 16)
     );
     port(
         clk    : in  bit_t;             -- The clock.
@@ -48,80 +50,181 @@ end acc;
 
 architecture rtl of acc is
 
--- All internal signals are defined here
-    type state_type is (idle, read, write, invert, done);
-    signal data_r, data_w, next_data_w : word_t;
-    signal next_reg_addr, reg_addr : halfword_t;
+    -- All internal signals are defined here
+    type state_type is (idle, read_R0, read_R1, read_R2, write, compute_edge, compute_edge_RP, done);
+    type pixel_matrix_type is array (0 to 2, 0 to 3) of std_logic_vector(7 downto 0);
+
+    signal next_dataW : word_t;
+    signal next_addr : halfword_t;
     signal state, next_state : state_type;
+    signal dx_LP, dx_RP, dy_LP, dy_RP : std_logic_vector(7 downto 0);
+    signal dn_LP, dn_RP : std_logic_vector(7 downto 0);
+    signal pixel_matrix : pixel_matrix_type;
+    signal next_col, next_row : unsigned(15 downto 0) := (others => '0');
+    signal col, row : unsigned(15 downto 0) := (others => '0');
+    
+    -- Define internal signals for addr and dataW
+    signal internal_addr : halfword_t := (others => '0');
+    signal internal_dataW : word_t := (others => '0');
+
+    constant TWO : signed(7 downto 0) := to_signed(2, 8);
 
 begin
 
-    cl : process(start, state, next_state, next_reg_addr, data_r, data_w, next_data_w)
+    -- Update the cl process to use internal signals
+    cl : process(start, state, next_state, next_addr, next_dataW)
     begin
         next_state <= state;
-        next_reg_addr <= reg_addr;
-        next_data_w <= data_w;
         en <= '0';
         we <= '0';
-        addr <= reg_addr;
-        dataW <= data_w;
-        
+
+        -- Use internal signals as default assignments
+        next_addr <= internal_addr;
+        next_dataW <= internal_dataW;
+        next_col <= col;
+        next_row <= row;
+        dx_LP <= (others => '0');
+        dx_RP <= (others => '0');
+        dy_LP <= (others => '0');
+        dy_RP <= (others => '0');
+        dn_LP <= (others => '0');
+        dn_RP <= (others => '0');
         
         case state is
 
             when idle =>
                 if start = '1' then
-                    next_state <= read;
+                    next_state <= read_R0;
                 end if;
 
-            when read =>
+            when read_R0 =>
                 en <= '1';
+                next_addr <= halfword_t(col + row * 88);
+                
+                pixel_matrix(0,0) <= dataR(31 downto 24);
+                pixel_matrix(0,1) <= dataR(23 downto 16);
+                pixel_matrix(0,2) <= dataR(15 downto 8);
+                pixel_matrix(0,3) <= dataR(7 downto 0);
+                
+                if (row < 2) then
+                    next_state <= read_R1;
+                else
+                    next_state <= compute_edge;    
+                end if;
+
+                if (row = 287) then
+                    next_col <= next_col + 1;
+                    next_row <= (others => '0');
+                else
+                    next_row <= next_row + 1;
+                end if;
+                
+            when read_R1 =>
+                en <= '1';
+                next_addr <= halfword_t(col + row * 88);
+
+                pixel_matrix(1,0) <= dataR(31 downto 24);
+                pixel_matrix(1,1) <= dataR(23 downto 16);
+                pixel_matrix(1,2) <= dataR(15 downto 8);
+                pixel_matrix(1,3) <= dataR(7 downto 0);
+                
+                next_state <= read_R2;
+                next_row <= next_row + 1;
+                
+            when read_R2 =>
+                en <= '1';
+                next_addr <= halfword_t(col + row * 88);
+                
+                pixel_matrix(2,0) <= dataR(31 downto 24);
+                pixel_matrix(2,1) <= dataR(23 downto 16);
+                pixel_matrix(2,2) <= dataR(15 downto 8);
+                pixel_matrix(2,3) <= dataR(7 downto 0);
+                
+                next_state <= compute_edge;
+                next_row <= next_row + 1;
+
+            when compute_edge =>
+                dx_LP <= std_logic_vector(
+                         signed(pixel_matrix(0,2)) - signed(pixel_matrix(0,0)) +
+                         TWO * (signed(pixel_matrix(1,2)) - signed(pixel_matrix(1,0))) +
+                         signed(pixel_matrix(2,2)) - signed(pixel_matrix(2,0))
+                         );
+
+                dy_LP <= std_logic_vector(
+                         signed(pixel_matrix(0,0)) - signed(pixel_matrix(2,0)) + 
+                         TWO * (signed(pixel_matrix(0,1)) - signed(pixel_matrix(2,1))) + 
+                         signed(pixel_matrix(0,2)) - signed(pixel_matrix(2,2))
+                         );
+
+                dn_LP <= std_logic_vector(
+                         abs(signed(dx_LP)) + abs(signed(dy_LP))
+                         );
+                      
+                dx_RP <= std_logic_vector(
+                         signed(pixel_matrix(0,3)) - signed(pixel_matrix(0,1)) +
+                         TWO * (signed(pixel_matrix(1,3)) - signed(pixel_matrix(1,1))) +
+                         signed(pixel_matrix(2,3)) - signed(pixel_matrix(2,1))
+                         );
+
+                dy_RP <= std_logic_vector(
+                         signed(pixel_matrix(0,1)) - signed(pixel_matrix(2,1)) + 
+                         TWO * (signed(pixel_matrix(0,2)) - signed(pixel_matrix(2,2))) + 
+                         signed(pixel_matrix(0,3)) - signed(pixel_matrix(2,3))
+                         );
+
+                dn_RP <= std_logic_vector(
+                         abs(signed(dx_RP)) + abs(signed(dy_RP))
+                         );
+
                 next_state <= write;
 
             when write =>
                 en <= '1';
                 we <= '1';
+
+                next_dataW <= pixel_matrix(1,0)   & 
+                              dn_LP               & 
+                              dn_RP               &
+                              pixel_matrix(1,3);
                 
-                next_data_w <=  std_logic_vector(255 - unsigned(dataR(31 downto 24))) & 
-                                std_logic_vector(255 - unsigned(dataR(23 downto 16))) & 
-                                std_logic_vector(255 - unsigned(dataR(15 downto 8)))  &
-                                std_logic_vector(255 - unsigned(dataR( 7 downto 0)));
+                next_addr <= halfword_t(col + (row - 1) * 88 + MAX_ADDR);
                 
-                next_reg_addr <= halfword_t(unsigned(reg_addr) + 1);
-                next_state <= read;
-                addr <= halfword_t(unsigned(reg_addr) + MAX_ADDR);
-                
-                
-                if (MAX_ADDR - unsigned(reg_addr) = 0) then
+                if row = MAX_ROW - 1 and col = MAX_COL - 1 then
                     finish <= '1';
                     next_state <= idle;
+                else
+                    next_state <= read_R0;
                 end if;
 
             when others =>
                 next_state <= idle;
-            
         end case;
-
-            
     end process cl;
 
-
--- Template for a process
-   seq : process(clk)
-   begin
-       if rising_edge(clk) then
-           if reset = '1' then
+    -- Update the seq process to use internal signals
+    seq : process(clk)
+    begin
+        if rising_edge(clk) then
+            if reset = '1' then
                 state <= idle;
-                reg_addr <= (others => '0');
-                data_r <= (others => '0');
-                data_w <= (others => '0');
-           else
+                internal_addr <= (others => '0');
+                internal_dataW <= (others => '0');
+                col <= (others => '0');
+                row <= (others => '0');
+                finish <= '0';
+                en <= '0';
+            else
                 state <= next_state;
-                reg_addr <= next_reg_addr;
-                data_w <= next_data_w;
+                internal_addr <= next_addr;
+                internal_dataW <= next_dataW;
+                col <= next_col;
+                row <= next_row;
+            end if;
+        end if;
+    end process seq;
 
-           end if;
-       end if;
-   end process seq;
+    -- Drive the output ports with internal signals
+    addr <= internal_addr;
+    dataW <= internal_dataW;
 
 end rtl;
